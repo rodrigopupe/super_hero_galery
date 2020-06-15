7b5404419b1af3a6b6d5cd46f727fe0a660a7e0b
Rodrigo Pupe <rodrigopupe@outlook.com>
2020-06-15 10:24:04 -0300

    Included all english translation.

--- lib/i18n/strings_en.dart
+++ lib/i18n/strings_en.dart
@@ -1,24 +1,42 @@
 const Map<String, String> ENGLISH = {
-  // HomePage
-  'choose_place_menu_label': 'Choose where to receive',
-  'take_away_menu_label': 'Take away',
-  'consume_local_menu_label': 'Consume on place (table)',
-  'delivery_any_menu_label': 'Delivery where I am',
-  'delivery_address_menu_label': 'Delivery in an address',
+  'app_title': 'Super Hero Galery',
+  
+  'super_heroes_count_label': 'Looking for heroes on multiverses: <#1>',
+  'search_box_hint': 'Name, alias or occupation',
+  'no_super_hero_found': 'No super hero found',
 
-  // HomeFrame
-  'home_frame_no_result': "There's no market close to your location.",
-  'home_frame_error': 'Error on listing all close markets. Please try again.',
+  'not_available_text': '-',
 
-  // ChooseDeliveryAddressPanel
-  'choose_delivery_address_panel_title': 'At what address do you want to receive?',
-  'choose_delivery_address_panel_error': 'Error on listing your delivery addresses. Please try again.',
+  'power_stats_label': 'Power statistics',
+  'intelligence_label': 'Intelligence',
+  'strength_label': 'Strength',
+  'speed_label': 'Speed',
+  'durability_label': 'Durability',
+  'power_label': 'Power',
+  'combat_label': 'Combat',
 
-  // ProfilePage
-  'profile_title': 'Profile',
+  'biography_label': 'Biography',
+  'fullname_label': 'Fullname',
+  'alter_egos_label': 'Alter egos',
+  'aliases_label': 'Aliases',
+  'place_birth_label': 'Place of birth',
+  'first_appearance_label': 'First appearance',
+  'publisher_label': 'Publisher',
+  'alignment_label': 'Alignment',
 
-  // UserOrdersPage
-  'user_orders_title': 'ORDERS',
-  'opened_orders_label':  'In progress',
-  'closed_orders_label':  'Finished',
+  'appearance_label': 'Appearance',
+  'gender_label': 'Gender',
+  'race_label': 'Race',
+  'height_label': 'Height',
+  'weight_label': 'Weight',
+  'eye_color_label': 'Eye color',
+  'hair_color_label': 'Hair color',
+
+  'work_label': 'Work',
+  'occupation_label': 'Occupation',
+  'base_label': 'Base',
+
+  'connections_label': 'Connections',
+  'group_affiliation_label': 'Group affiliation',
+  'relatives_label': 'Relatives',
 };
